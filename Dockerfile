71f0e4136cec2f8c45a48b1169a1ea907af7748e
javakhiryu <javakhiryulchibaev@gmail.com>
2025-05-06 14:36:50 +0500

    -fix: base-url, db migration

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,18 @@
 FROM golang:1.24.1-alpine3.21 AS builder
-
 WORKDIR /app
-
 COPY . .
-
 RUN go build -o main main.go
 RUN apk add curl
 
-
 FROM alpine:3.21
-
 WORKDIR /app
-
 COPY --from=builder /app/main .
 COPY app.env .
 COPY start.sh .
 COPY wait-for.sh .
 COPY db/migration ./db/migration
-
-RUN chmod +x start.sh wait-for.sh
-
+RUN chmod +x start.sh wait-for.sh && \
+    apk add --no-cache postgresql-client # Для проверки БД
 EXPOSE 8080
-
-CMD [ "/app/main" ]
-ENTRYPOINT [ "/app/start.sh" ]
+ENTRYPOINT ["/app/start.sh"]
+CMD ["/app/main"]
--- api/pages.go
+++ api/pages.go
@@ -7,47 +7,62 @@ import (
 )
 
 func (server *Server) renderIndexPage(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "index.html", nil)
+	ctx.HTML(http.StatusOK, "index.html", gin.H{
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
+	})
 }
 
 func (server *Server) renderSellRequestPage(ctx *gin.Context) {
 	id := ctx.Param("id")
 	ctx.HTML(http.StatusOK, "sell_request_page.html", gin.H{
 		"id": id,
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
 	})
 }
 
 func (server *Server) renderCreateSellRequestPage(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "create_sell_request_page.html", nil)
+	ctx.HTML(http.StatusOK, "create_sell_request_page.html",  gin.H{
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
+	})
 }
 
 func (server *Server) renderListSellRequestsPage(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "list_sell_requests_page.html", nil)
+	ctx.HTML(http.StatusOK, "list_sell_requests_page.html",  gin.H{
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
+	})
 }
 
 func (server *Server) renderCreateBuyRequestPage(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "create_buy_request_page.html", nil)
+	ctx.HTML(http.StatusOK, "create_buy_request_page.html",  gin.H{
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
+	})
 }
 
 func (server *Server) renderBuyRequestPage(ctx *gin.Context) {
 	id := ctx.Param("id")
 	ctx.HTML(http.StatusOK, "buy_request_page.html", gin.H{
 		"id": id,
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
 	})
 }
 
 func (server *Server) renderListBuyRequestsPage(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "list_buy_requests_page.html", nil)
+	ctx.HTML(http.StatusOK, "list_buy_requests_page.html",  gin.H{
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
+	})
 }
 
 func (server *Server) renderAuthPage(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "auth_page.html", nil)
+	ctx.HTML(http.StatusOK, "auth_page.html",  gin.H{
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
+	})
 }
 
 func (server *Server) renderListSpacesPage(ctx *gin.Context) {
 	id := ctx.Param("id")
 	ctx.HTML(http.StatusOK, "list_spaces_page.html", gin.H{
 		"id": id,
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
 	})
 }
 
@@ -55,5 +70,6 @@ func (server *Server) renderCreateSpacePage(ctx *gin.Context) {
 	id := ctx.Param("id")
 	ctx.HTML(http.StatusOK, "create_space_page.html", gin.H{
 		"id": id,
+		"BaseUrl": ctx.MustGet("BaseUrl").(string),
 	})
 }
